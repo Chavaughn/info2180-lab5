ed30f58e7fa856341ef946cc155aeb29ccd02e0d
Chavvv <chavaughnwilkins@gmail.com>
2021-11-09 20:31:07 -0500

    Functionality and minor cs

---
+++ world.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', () => {
+
+    let sanitizeUrl;
+    let sanitizeVal;
+
+    let heading = document.querySelector("header h1");
+    heading.style.color = "#fff";
+    heading.style.transition = "all 2s ease-in-out";
+
+    let resultDiv = document.querySelector("div#result");
+    document.querySelector("button#lookup").addEventListener("click", (event) => {
+        sanitizeVal = document.querySelector("input#country").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g, '');
+        sanitizeUrl = `world.php?country= ${sanitizeVal}`.replace(/"[^-0-9+@#/%?~_|!:,.;\(\)]"/g, '');
+        ajaxCall(event);
+
+    });
+
+    document.querySelector("button#citylookup").addEventListener("click", (event) => {
+        sanitizeVal = document.querySelector("input#country").value.replace(/[-&\/\\#,+()$@|~%!.'":;*?<>{}]/g, '');
+        sanitizeUrl = `world.php?country= ${sanitizeVal}&context=cities`.replace(/"[^-0-9+@#/%?~_|!:,.;\(\)]"/g, '');
+        ajaxCall(event);
+    });
+
+    let ajaxCall = (event) => {
+        event.preventDefault();
+        fetch(sanitizeUrl, { method: 'GET' })
+            .then(resp => resp.text())
+            .then(info => {
+                resultDiv.innerHTML = '';
+                resultDiv.innerHTML = info;
+            })
+    }
+
+
+});
